18c863eb8aa58f61257ddcab34e81baa2a19e541
Alexander Ivanov <76427034+Apfe1Saft@users.noreply.github.com>
2023-07-12 07:38:50 +0000

    Update 11 files

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
 import "./styles.css";
+const button = document.getElementById("my-button");
+button.addEventListener("click", clickFunction);
 
-document.getElementById("app").innerHTML = `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>Hello World Example</title>
-  </head>
-  <body>
-    <h1>Hello world</h1>
+let heading = document.getElementById("my-heading");
+const list = document.getElementById("my-list");
+let message = document.getElementById("my-textarea");
 
-    <button id="my-button">Click me!</button>
-
-    <ul id="my-list"></ul>
-
-    <textarea id="my-textarea"></textarea>
-
-    <button id="add-data">Add to List</button>
-
-    <script>
-      const button = document.getElementById("my-button");
-      button.addEventListener("click", function () {
-        console.log("Hello world");
-      });
-
-      const addDataButton = document.getElementById("add-data");
-      const myList = document.getElementById("my-list");
-      addDataButton.addEventListener("click", function () {
-        const textarea = document.getElementById("my-textarea");
-        const listItem = document.createElement("li");
-        listItem.textContent = textarea.value;
-        myList.appendChild(listItem);
-      });
-    </script>
-  </body>
-</html>
-`;
+function clickFunction() {
+  console.log("hello world");
+  heading.innerHTML = "My notebook";
+  let li = document.createElement("li");
+  li.innerText = message.value;
+  list.appendChild(li);
+}
