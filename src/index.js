01ce92f2c7f4a5ad09aa9576501a7fffcde0648e
Alexander Ivanov <76427034+Apfe1Saft@users.noreply.github.com>
2023-07-12 07:21:41 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<!DOCTYPE html>
+document.getElementById("app").innerHTML = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8" />
     <title>Hello World Example</title>
   </head>
   <body>
-    <h1 id="my-heading">Hello world</h1>
+    <h1>Hello world</h1>
 
     <button id="my-button">Click me!</button>
 
@@ -19,21 +18,14 @@ document.getElementById("app").innerHTML = `
     <button id="add-data">Add to List</button>
 
     <script>
-      // Task 2: Creating a button
       const button = document.getElementById("my-button");
       button.addEventListener("click", function () {
         console.log("Hello world");
-
-        // Task 3: Button DOM changes
-        const heading = document.getElementById("my-heading");
-        heading.textContent = "My notebook";
       });
 
-      // Task 4: Unordered list
       const addDataButton = document.getElementById("add-data");
       const myList = document.getElementById("my-list");
       addDataButton.addEventListener("click", function () {
-        // Task 5: Custom text to list
         const textarea = document.getElementById("my-textarea");
         const listItem = document.createElement("li");
         listItem.textContent = textarea.value;
@@ -41,4 +33,5 @@ document.getElementById("app").innerHTML = `
       });
     </script>
   </body>
+</html>
 `;
